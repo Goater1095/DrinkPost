6a944350a7b52d564f46a9dde304c0059fb10240
goater <goater1095@gmail.com>
2021-01-21 17:49:43 +0800

    modify main.js

--- main.js
+++ main.js
@@ -22,15 +22,34 @@ Drink.prototype.price = function () {
   }
 };
 
+function AlphaPos() {}
+// Constructor function for Alpha Pos System
+AlphaPos.prototype.getCheckedValue = function (inputName) {
+  let selectedOption = '';
+  document.querySelectorAll(`[name=${inputName}]`).forEach(function (item) {
+    if (item.checked) {
+      selectedOption = item.value;
+    }
+  });
+  return selectedOption;
+};
+
+// new the alphaPos Instance
+const alphaPos = new AlphaPos();
+
 const addDrinkButton = document.querySelector('[data-alpha-pos="add-drink"]');
 addDrinkButton.addEventListener('click', function () {
-  console.log('click');
+  // 1. 取得店員選擇的飲料品項、甜度、冰塊選項內容
+  const drinkName = alphaPos.getCheckedValue('drink');
+  const ice = alphaPos.getCheckedValue('ice');
+  const sugar = alphaPos.getCheckedValue('sugar');
+  console.log(`${drinkName}, ${ice}, ${sugar}`);
+  // 2. 如果沒有選擇飲料品項，跳出提示
+  if (!drinkName) {
+    alert('Please choose at least one item.');
+    return;
+  }
+  // 3. 建立飲料實例，並取得飲料價格
+  const drink = new Drink();
+  // 4. 將飲料實例產生成左側訂單區的畫面
 });
-
-function AlphaPos() {}
-
-let allDrinksOptions = document.querySelectorAll('input[name="drink"]');
-console.log(allDrinksOptions);
-
-let oolongTea = allDrinksOptions[1]; // Oolong Tea is the second option, so use index '1'
-console.log(oolongTea.checked);
